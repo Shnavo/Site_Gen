fdb03217ff1efedee96d21269a292cae887d98db
Shnavo <szango297@gmail.com>
2025-11-06 00:08:41 +0100

    Added childless LeafNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -35,3 +35,22 @@ class HTMLNode():
             and self.children == other.children
             and self.props == other.props
         )
+
+class LeafNode(HTMLNode):
+    def __init__(
+                self, 
+                tag: str, 
+                value: str, 
+                props: dict | None = None
+    ):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError("All leaf nodes must have a value")
+        if self.tag == None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
